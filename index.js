eec473b809a62854da36bc20a0893a584ab054df
Maaz Ahmed <ahmedmaaz73@gmail.com>
2024-01-09 20:22:11 +0530

    Added Cancel operation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import {intro, outro, spinner, text, confirm} from '@clack/prompts';
+import {intro, outro, spinner, text, confirm, isCancel, cancel} from '@clack/prompts';
 import generator from 'generate-password';
 import { setTimeout } from 'node:timers/promises';
 
@@ -11,9 +11,19 @@ async function main(){
         message: 'Enter the length of password: '
     });
 
+    if (isCancel(length)) {
+        cancel('Operation cancelled.');
+        process.exit(0);
+    }
+
     const value = await confirm({
         message: 'Do you want to include special characters?'
     })
+
+    if (isCancel(value)) {
+        cancel('Operation cancelled.');
+        process.exit(0);
+    }
 
     const password = generator.generate({
         length: length,
